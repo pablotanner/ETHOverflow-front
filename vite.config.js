5ed297bfffd2288de7065f80e1410c5196fc7cad
Pablo Tanner <pablo.tanner@uzh.ch>
2024-10-11 22:57:28 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "tailwindcss";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3000
+    port: 5173
   },
   plugins: [react(), tailwindcss()],
 })
