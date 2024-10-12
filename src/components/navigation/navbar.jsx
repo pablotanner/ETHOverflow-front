df4f7fc1d49ccd3470f10b71486abba2dcc9f35e
Pablo Tanner <pablo.tanner@uzh.ch>
2024-10-12 21:17:45 +0200

    Update navbar.jsx

--- src/components/navigation/navbar.jsx
+++ src/components/navigation/navbar.jsx
@@ -26,17 +26,17 @@ const Navbar = () => {
     }
 
     return (
-        <nav className="px-12 py-4 border-b border-b-gray-200 flex flex-row gap-6 md:gap-16 lg:gap-24 items-center justify-center w-full">
+        <nav className="px-12 py-4 border-b border-b-gray-200 flex flex-row gap-6 md:gap-8 lg:gap-18 xl:gap-24 items-center justify-center w-full overflow-auto">
             <div>
-                <img src={"/logo.png"} className="hover:opacity-60 cursor-pointer" width={100} onClick={() => navigate("/")}/>
+                <img src={"/logo.png"} className="hover:opacity-60 cursor-pointer min-w-16 md:min-w-24 lg:min-w-32" width={100} onClick={() => navigate("/")}/>
             </div>
 
             <MenuItem link={"/"}>Home</MenuItem>
             <MenuItem link={"/about"}>About</MenuItem>
             <MenuItem link={"/courses"}>Courses</MenuItem>
-            <div className="relative">
+            <div className="relative hidden sm:flex">
                 <MagnifyingGlassIcon className="absolute top-1/2 left-2 transform -translate-y-1/2 text-gray-400"/>
-                <Input className="h-8 pl-8"/>
+                <Input className="h-8 pl-8 min-w-24 md:min-w-36"/>
             </div>
 
             <MenuItem link={"/badges"}>Achievements</MenuItem>
