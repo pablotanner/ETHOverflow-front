281e4ee4cef4efe3439d7906b8d614ac1f35ba9e
Pablo Tanner <pablo.tanner@uzh.ch>
2024-10-12 22:59:03 +0200

    Update ratings.jsx

--- src/components/ratings/ratings.jsx
+++ src/components/ratings/ratings.jsx
@@ -12,7 +12,7 @@ const Ratings = ({ rating, question, answer, direction }) => {
 
     const [sendVoteAnswer] = useCreateAnswerVoteMutation();
 
-    console.log(answer.id)
+    console.log(answer)
     if (direction === 'horizontal') {
         return (
             <div className="flex items-center justify-start gap-[2px]">
