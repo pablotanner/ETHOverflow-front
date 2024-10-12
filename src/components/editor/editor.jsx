a0f117f2c473211bdd3d86bb787ca4ae2dd7d88f
Pablo Tanner <pablo.tanner@uzh.ch>
2024-10-12 17:39:31 +0200

    Try adding formla

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -1,5 +1,16 @@
 import ReactQuill from "react-quill";
 
+import katex from 'katex';
+import 'katex/dist/katex.min.css';
+
+const formula = () => {
+    const toolbar = this.quill.getModule('toolbar');
+    toolbar.addHandler('formula', () => {
+        const value = prompt('Enter formula');
+        this.quill.focus();
+        this.quill.insertText(this.quill.getLength(), `\\(${value}\\)`);
+    });
+}
 
 const Editor = ({value, setValue}) => {
 
