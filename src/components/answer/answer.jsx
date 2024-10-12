cd951090b36103e59ff08280b5886e96fa9b8256
Pablo Tanner <pablo.tanner@uzh.ch>
2024-10-13 01:26:41 +0200

    Update answer.jsx

--- src/components/answer/answer.jsx
+++ src/components/answer/answer.jsx
@@ -57,7 +57,7 @@ const Answer = ({ answer, question, comments }) => {
 
                 <HoverCard>
                     <HoverCardTrigger asChild>
-                        <p className="text-gray-500 text-xs">{answer?.created_by}</p>
+                        <p className="text-gray-500 text-xs cursor-pointer">{answer?.created_by}</p>
                     </HoverCardTrigger>
                     <HoverCardContent className="w-80">
                     <div className="flex justify-between space-x-4">
@@ -67,14 +67,14 @@ const Answer = ({ answer, question, comments }) => {
                                 </AvatarFallback>
                             </Avatar>
                             <div className="space-y-1">
-                                <h4 className="text-sm font-semibold">@nextjs</h4>
+                                <h4 className="text-sm font-semibold">{answer?.creator?.display_name}</h4>
                                 <p className="text-sm">
-                                    The React Framework – created and maintained by @vercel.
+                                    {answer?.creator?.email} - {answer?.creator?.reputation} Reputation
                                 </p>
                                 <div className="flex items-center pt-2">
                                     <CalendarDays className="mr-2 h-4 w-4 opacity-70" />{" "}
                                     <span className="text-xs text-muted-foreground">
-                                Joined December 2021
+                                        Joined on {formatDate(answer?.creator?.date_joined)}
                                 </span>
                                 </div>
                             </div>
