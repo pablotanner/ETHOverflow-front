8479d8f2a9730251d6a67edab50c64cc0e767a07
Pablo Tanner <pablo.tanner@uzh.ch>
2024-10-13 01:23:48 +0200

    Update answer.jsx

--- src/components/answer/answer.jsx
+++ src/components/answer/answer.jsx
@@ -16,6 +16,7 @@ import {useState} from "react";
 import {useCreateCommentMutation, useEditAnswerMutation} from "../../services/api/questionApi.js";
 import Editor from "../editor/editor.jsx";
 import {useGetUserActivityQuery} from "../../services/api/authApi.js";
+import {HoverCard, HoverCardContent, HoverCardTrigger} from "../card/hover-card.tsx";
 
 
 const Answer = ({ answer, question, comments }) => {
@@ -52,7 +53,33 @@ const Answer = ({ answer, question, comments }) => {
                         <p>{answer?.creator?.display_name[0] || "?"}</p>
                     </AvatarFallback>
                 </Avatar>
-                <p className="text-gray-500 text-xs">{answer?.created_by}</p>
+
+                <HoverCard>
+                    <HoverCardTrigger asChild>
+                        <p className="text-gray-500 text-xs">{answer?.created_by}</p>
+                    </HoverCardTrigger>
+                    <HoverCardContent className="w-80">
+                    <div className="flex justify-between space-x-4">
+                            <Avatar>
+                                <AvatarFallback>
+                                    <p>{answer?.creator?.display_name[0] || "?"}</p>
+                                </AvatarFallback>
+                            </Avatar>
+                            <div className="space-y-1">
+                                <h4 className="text-sm font-semibold">@nextjs</h4>
+                                <p className="text-sm">
+                                    The React Framework – created and maintained by @vercel.
+                                </p>
+                                <div className="flex items-center pt-2">
+                                    <CalendarDays className="mr-2 h-4 w-4 opacity-70" />{" "}
+                                    <span className="text-xs text-muted-foreground">
+                                Joined December 2021
+                                </span>
+                                </div>
+                            </div>
+                        </div>
+                    </HoverCardContent>
+                </HoverCard>
             </div>
 
             <div className="flex items-center text-sm ml-7"
