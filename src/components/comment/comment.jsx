597044a77d9f8e9b80b96ed9b94a291d8400bb79
Pablo Tanner <pablo.tanner@uzh.ch>
2024-10-13 01:28:51 +0200

    Update comment.jsx

--- src/components/comment/comment.jsx
+++ src/components/comment/comment.jsx
@@ -1,10 +1,38 @@
+import {HoverCard, HoverCardContent, HoverCardTrigger} from "../card/hover-card.tsx";
+import {Avatar, AvatarFallback} from "../avatar/avatar.tsx";
+import {CalendarDays} from "lucide-react";
 
 
 const Comment = ({ comment }) => {
     return (
         <div className="text-xs inline-flex float-left m-0 gap-1">
             <p className="text-gray-500">
-                {comment?.created_by}:
+                <HoverCard>
+                    <HoverCardTrigger asChild>
+                        <p className="text-gray-500 text-xs cursor-pointer">{comment?.created_by}:</p>
+                    </HoverCardTrigger>
+                    <HoverCardContent className="w-80">
+                        <div className="flex justify-between space-x-4">
+                            <Avatar>
+                                <AvatarFallback>
+                                    <p>{comment?.creator?.display_name[0] || "?"}</p>
+                                </AvatarFallback>
+                            </Avatar>
+                            <div className="space-y-1">
+                                <h4 className="text-sm font-semibold">{comment?.creator?.display_name}</h4>
+                                <p className="text-sm">
+                                    Reputation: {comment?.creator?.reputation}
+                                </p>
+                                <div className="flex items-center pt-2">
+                                    <CalendarDays className="mr-2 h-4 w-4 opacity-70" />{" "}
+                                    <span className="text-xs text-muted-foreground">
+                                        Joined on {formatDate(comment?.creator?.date_joined)}
+                                </span>
+                                </div>
+                            </div>
+                        </div>
+                    </HoverCardContent>
+                </HoverCard>
             </p>
             <p>{comment.content}</p>
         </div>
