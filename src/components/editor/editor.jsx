9087e974e4a2121e1720c197b90b3e1e70da7af9
Pablo Tanner <pablo.tanner@uzh.ch>
2024-10-12 17:58:27 +0200

    Update editor.jsx

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -10,17 +10,7 @@ import {useEffect, useRef} from "react";
 const Editor = ({value, setValue}) => {
     const quillRef = useRef(null); // Create a Ref
 
-    useEffect(() => {
-        if (quillRef != null && quillRef.current != null) {
-            const toolbar = quillRef.current.getEditor().getModule("toolbar");
-            toolbar.addHandler("formula", function() {
-                const value = prompt('Enter formula');
-                this.quill.focus();
-                let cursorPosition = this.quill.getSelection().index;
-                this.quill.insertEmbed(cursorPosition, 'formula', value);
-            });
-        }
-    }, []);
+
     return (
         <ReactQuill
             theme="snow"
