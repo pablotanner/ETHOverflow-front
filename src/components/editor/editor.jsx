60e152df5993672cbccde1f139aaafdb0a84d9f1
Pablo Tanner <pablo.tanner@uzh.ch>
2024-10-12 17:36:52 +0200

    Update editor.jsx

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -11,12 +11,10 @@ const Editor = ({value, setValue}) => {
 
             modules={{
                 toolbar: [
-                    [{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
                     [{size: []}],
-                    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-                    [{'list': 'ordered'}, {'list': 'bullet'},
-                        {'indent': '-1'}, {'indent': '+1'}],
-                    ['link', 'image', 'video'],
+                    ['bold', 'italic', 'underline', 'strike'],
+                    [{'list': 'ordered'}, {'list': 'bullet'},],
+                    ['formula'],
                     ['clean']
                 ],
             }}
