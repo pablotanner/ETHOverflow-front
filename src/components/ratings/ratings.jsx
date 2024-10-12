d412bf50069c4f02c0d1a222e8896542ad9d8671
Pablo Tanner <pablo.tanner@uzh.ch>
2024-10-12 22:51:03 +0200

    Update ratings.jsx

--- src/components/ratings/ratings.jsx
+++ src/components/ratings/ratings.jsx
@@ -16,6 +16,19 @@ const Ratings = ({ rating, question, direction }) => {
             <div className="flex items-center justify-start gap-[2px]">
                 <ArrowBigUp
 
+
+                    className="text-gray-400 hover:text-indigo-400 hover:cursor-pointer" width={22}/>
+                <p className="font-semibold text-gray-700 text-xs">{rating || 0}</p>
+                <ArrowBigDown
+
+                    className="text-gray-400 hover:text-red-400 hover:cursor-pointer"  width={22}/>
+            </div>
+        )
+    }
+    else {
+        return (
+            <div className="flex flex-col items-center justify-start">
+                <ArrowUp
                     onClick={() => {
                         sendVote({
                             question_id: question?.id,
@@ -25,9 +38,9 @@ const Ratings = ({ rating, question, direction }) => {
                         })
                     }}
 
-                    className="text-gray-400 hover:text-indigo-400 hover:cursor-pointer" width={22}/>
-                <p className="font-semibold text-gray-700 text-xs">{rating || 0}</p>
-                <ArrowBigDown
+                    className="text-primary-foreground hover:text-indigo-300 hover:cursor-pointer" />
+                <p className="font-bold text-lg">{rating || 0}</p>
+                <ArrowDown
                     onClick={() => {
                         sendVote({
                             question_id: question?.id,
@@ -36,17 +49,7 @@ const Ratings = ({ rating, question, direction }) => {
                             }
                         })
                     }}
-
-                    className="text-gray-400 hover:text-red-400 hover:cursor-pointer"  width={22}/>
-            </div>
-        )
-    }
-    else {
-        return (
-            <div className="flex flex-col items-center justify-start">
-                <ArrowUp className="text-primary-foreground hover:text-indigo-300 hover:cursor-pointer" />
-                <p className="font-bold text-lg">{rating || 0}</p>
-                <ArrowDown className="text-primary-foreground hover:text-red-400 hover:cursor-pointer"/>
+                    className="text-primary-foreground hover:text-red-400 hover:cursor-pointer"/>
             </div>
         )
     }
